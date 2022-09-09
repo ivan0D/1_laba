ff6b1ea6ebe36643d2c8d80869c4671786963abf
Ivan <russkov031@yandex.ru>
2022-09-09 21:12:38 +0300

    пункт 4

--- main.cpp
+++ main.cpp
@@ -3,57 +3,29 @@
 #include <iomanip>
 using namespace std;
 
-typedef struct {
-    char* data; // 4
-    int i; // 4
+struct A{
+    int a; // 4
+    char c; // 1
+    int f; // 4
     double d; // 8
-} S;
+}; // 17
 
-enum ERROR : short {
-    BAD_ALLOC,
-    OVERFLOW_ERROR,
-    RUNTIME_ERROR
-};
-
-enum EVENT{
-    A = 10,
-    B,
-    C = 12
-};
+/*
+ *
+ *#[a][a][a][a][c][][][]#[f][f][f][f][][][][]#[d][d][d][d][d][d][d][d]#[][][][][][][][]#
+ *
+ * 
+ */
 
 int main() {
 
-    cout << "\t Type name\tSize(bytes)" << endl;
-    cout << setw(17) << typeid(EVENT).name() << setw(15) << sizeof(EVENT) << endl;
-    cout << setw(17) << typeid(ERROR).name() << setw(15) << sizeof(ERROR) << endl << endl;
-
-    EVENT e = EVENT ::A;
-    EVENT e1 = EVENT ::B;
-    EVENT e2 = EVENT ::C;
-
-    cout << "EVENT::A = " << e << endl;
-    cout << "EVENT::B = " << e1 << endl;
-    cout << "EVENT::C = " << e2 << endl << endl;
-
-    int e_ = e;
-    int e1_ = e1;
-    int e2_ = e2;
-
-    // e = e_; // invalid conversion from 'int' to 'EVENT'
-    e = static_cast<EVENT>(e_); // вот так надо!
-    cout << "e = " << e << endl;
-
-    e_ = 1;
-    cout << "e_ = " << e_ << endl;
-
-    // e = e * e1; // e * e1 - int
-    e_ = e * e1;
-    e_ = e / e1;
-    e_ = e + e1;
-    e_ = e - e1;
-
-
-
+    A a{0, 0, 0, 0};
+    cout << "sizeof(A.a) = " << sizeof(a.a) << endl;
+    cout << "sizeof(A.c) = " << sizeof(a.c) << endl;
+    cout << "sizeof(A.f) = " << sizeof(a.f) << endl;
+    cout << "sizeof(A.d) = " << sizeof(a.d) << endl;
+    cout << "Total: " << sizeof(a.a) + sizeof(a.c) + sizeof(a.f) + sizeof(a.d) << endl;
+    cout << "sizeof(A) = " << sizeof(A) << endl;
 
     return 0;
 }
