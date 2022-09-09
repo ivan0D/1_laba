f66aa758b26b73b87dfac193a6a036461cf84d71
Ivan <russkov031@yandex.ru>
2022-09-09 20:52:20 +0300

    пункт 3

--- main.cpp
+++ main.cpp
@@ -9,49 +9,51 @@ typedef struct {
     double d; // 8
 } S;
 
-typedef unsigned long long int ULLINT;
-typedef long long int LLINT;
-typedef unsigned char UCHAR;
-typedef signed char SCHAR;
-typedef short int SINT;
-typedef unsigned int UINT;
-typedef unsigned long ULONG;
-typedef long double LDOUBLE;
+enum ERROR : short {
+    BAD_ALLOC,
+    OVERFLOW_ERROR,
+    RUNTIME_ERROR
+};
+
+enum EVENT{
+    A = 10,
+    B,
+    C = 12
+};
 
 int main() {
 
-    char c;
-    SCHAR sc;
-    UCHAR uc;
-    SINT si;
-    UINT ui;
-    int i;
-    long l;
-    ULONG ul;
-    LLINT lli;
-    ULLINT ulli;
-    float f;
-    double d;
-    LDOUBLE ld;
-    bool b;
-    S s;
-
     cout << "\t Type name\tSize(bytes)" << endl;
-    cout << setw(17) << typeid(c).name() << setw(15) << sizeof(c) << endl;
-    cout << setw(17) << typeid(sc).name() << setw(15) << sizeof(sc) << endl;
-    cout << setw(17) << typeid(uc).name() << setw(15) << sizeof(uc) << endl;
-    cout << setw(17) << typeid(si).name() << setw(15) << sizeof(si) << endl;
-    cout << setw(17) << typeid(ui).name() << setw(15) << sizeof(ui) << endl;
-    cout << setw(17) << typeid(i).name() << setw(15) << sizeof(i) << endl;
-    cout << setw(17) << typeid(l).name() << setw(15) << sizeof(l) << endl;
-    cout << setw(17) << typeid(ul).name() << setw(15) << sizeof(ul) << endl;
-    cout << setw(17) << typeid(lli).name() << setw(15) << sizeof(lli) << endl;
-    cout << setw(17) << typeid(ulli).name() << setw(15) << sizeof(ulli) << endl;
-    cout << setw(17) << typeid(f).name() << setw(15) << sizeof(f) << endl;
-    cout << setw(17) << typeid(d).name() << setw(15) << sizeof(d) << endl;
-    cout << setw(17) << typeid(ld).name() << setw(15) << sizeof(ld) << endl;
-    cout << setw(17) << typeid(b).name() << setw(15) << sizeof(b) << endl;
-    cout << setw(17) << typeid(s).name() << setw(15) << sizeof(s) << endl;
+    cout << setw(17) << typeid(EVENT).name() << setw(15) << sizeof(EVENT) << endl;
+    cout << setw(17) << typeid(ERROR).name() << setw(15) << sizeof(ERROR) << endl << endl;
+
+    EVENT e = EVENT ::A;
+    EVENT e1 = EVENT ::B;
+    EVENT e2 = EVENT ::C;
+
+    cout << "EVENT::A = " << e << endl;
+    cout << "EVENT::B = " << e1 << endl;
+    cout << "EVENT::C = " << e2 << endl << endl;
+
+    int e_ = e;
+    int e1_ = e1;
+    int e2_ = e2;
+
+    // e = e_; // invalid conversion from 'int' to 'EVENT'
+    e = static_cast<EVENT>(e_); // вот так надо!
+    cout << "e = " << e << endl;
+
+    e_ = 1;
+    cout << "e_ = " << e_ << endl;
+
+    // e = e * e1; // e * e1 - int
+    e_ = e * e1;
+    e_ = e / e1;
+    e_ = e + e1;
+    e_ = e - e1;
+
+
+
 
     return 0;
 }
